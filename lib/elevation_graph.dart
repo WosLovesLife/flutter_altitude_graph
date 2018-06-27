87fe42c4146290b3f49a8701c1753243a037ef3b
WosLovesLife <zhang00h00@126.com>
2018-06-27 16:03:37 +0800

    绘制背景和简单线条

--- lib/elevation_graph.dart
+++ lib/elevation_graph.dart
@@ -31,7 +31,7 @@ class ElevationPainter extends CustomPainter {
 
   Paint mLevelLinePaint = Paint()
     ..color = Colors.amber
-    ..strokeWidth = 3.0
+    ..strokeWidth = 1.0
     ..style = PaintingStyle.stroke;
 
   ElevationPoint lastPoint;
@@ -40,23 +40,66 @@ class ElevationPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-//    canvas.drawLine(Offset(0.0, 500.0), Offset(350.0, 500.0), mLevelLinePaint);
-//    canvas.drawLine(Offset(0.0, 400.0), Offset(350.0, 400.0), mLevelLinePaint);
-//    canvas.drawLine(Offset(0.0, 300.0), Offset(350.0, 300.0), mLevelLinePaint);
-//    canvas.drawLine(Offset(0.0, 200.0), Offset(350.0, 200.0), mLevelLinePaint);
-//    canvas.drawLine(Offset(0.0, 100.0), Offset(350.0, 100.0), mLevelLinePaint);
-    canvas.save();
-    canvas.drawPath(dottedLine(350, 500.0, 3.0, 3.0), mLevelLinePaint);
-    canvas.restore();
-//    canvas.drawPath(dottedLine(350, 400.0, 3.0, 3.0), mLevelLinePaint);
-//    canvas.drawPath(dottedLine(350, 300.0, 3.0, 3.0), mLevelLinePaint);
-//    canvas.drawPath(dottedLine(350, 200.0, 3.0, 3.0), mLevelLinePaint);
-//    canvas.drawPath(dottedLine(350, 100.0, 3.0, 3.0), mLevelLinePaint);
+    drawAxis(canvas, size);
 
+    drawLines(size, canvas);
+  }
+
+  /// 绘制背景数轴
+  void drawAxis(Canvas canvas, Size size) {
+    canvas.save();
+    canvas.drawPath(dottedLine(330.0, 100.0, 2.0, 2.0), mLevelLinePaint);
+    generateLevelText("7000")
+      ..layout()
+      ..paint(canvas, Offset(330.0, 95.0));
+    canvas.drawPath(dottedLine(330.0, 200.0, 2.0, 2.0), mLevelLinePaint);
+    generateLevelText("6000")
+      ..layout()
+      ..paint(canvas, Offset(330.0, 195.0));
+    canvas.drawPath(dottedLine(330.0, 300.0, 2.0, 2.0), mLevelLinePaint);
+    generateLevelText("5000")
+      ..layout()
+      ..paint(canvas, Offset(330.0, 295.0));
+    canvas.drawPath(dottedLine(330.0, 400.0, 2.0, 2.0), mLevelLinePaint);
+    generateLevelText("4000")
+      ..layout()
+      ..paint(canvas, Offset(330.0, 395.0));
+    canvas.drawPath(dottedLine(330.0, 500.0, 2.0, 2.0), mLevelLinePaint);
+    generateLevelText("3000")
+      ..layout()
+      ..paint(canvas, Offset(330.0, 495.0));
+    canvas.restore();
+  }
+
+  Path dottedLine(double width, double y, double cutWidth, double interval) {
+    var path = Path();
+    var d = width / (cutWidth + interval);
+    path.moveTo(0.0, y);
+    for (int i = 0; i < d; i++) {
+      path.relativeLineTo(cutWidth, 0.0);
+      path.relativeMoveTo(interval, 0.0);
+    }
+    return path;
+  }
+
+  TextPainter generateLevelText(String text) {
+    return TextPainter(
+      textAlign: TextAlign.left,
+      textDirection: TextDirection.ltr,
+      text: TextSpan(
+        text: text,
+        style: TextStyle(
+          color: Colors.blueGrey,
+          fontSize: 10.0,
+        ),
+      ),
+    );
+  }
+
+  /// 绘制海拔图连线部分
+  void drawLines(Size size, Canvas canvas) {
     double ratioX = size.width * 1.0 / elevationPointList.last.point.dx;
     double ratioY = size.height / 1000.0;
-
-    print("ratioX = $ratioX; ratioY = $ratioY");
 
     var firstPoint = elevationPointList.first.point;
     var path = Path();
@@ -68,17 +111,6 @@ class ElevationPainter extends CustomPainter {
     canvas.save();
     canvas.drawPath(path, mLinePaint);
     canvas.restore();
-  }
-
-  Path dottedLine(int width, double y, double cutWidth, double interval) {
-    var path = Path();
-    var d = (cutWidth + interval) / width;
-    path.moveTo(0.0, y);
-    for (int i = 0; i < d; i++) {
-      path.lineTo(i * cutWidth, y);
-//      path.relativeMoveTo(interval, y);
-    }
-    return path;
   }
 
   @override
